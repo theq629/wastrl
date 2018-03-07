f9069f8544f1ef25c170a998893f765076d9a3ad
Max Whitney <mwhitney@alumni.sfu.ca>
2018-03-07 22:17:40 +0800

    Adjusted dialog colours to match map display.

--- wastrl/ui/basic/colours.py
+++ wastrl/ui/basic/colours.py
@@ -1,7 +1,7 @@
-title = 0xffff00
-text = 0xaaaaaa
-menu_key = 0xaaaa00
-action_command = 0x999999
-action_key = 0xaaaa00
-action_sep = 0x999999
+title = 0xffcf6d
+text = 0xcfc19a
+menu_key = 0x10ad80
+action_command = 0x10ad80
+action_key = 0xffcf6d
+action_sep = 0x10ad80
 keys_background = 0x222222
