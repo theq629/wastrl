2c38a87462ba370d027e4fba0ecd4c9ff4b236b1
Max Whitney <mwhitney@alumni.sfu.ca>
2018-03-08 12:28:02 +0800

    Refactored things creation to use templates.

--- wastrl/game/things.py
+++ wastrl/game/things.py
@@ -1,50 +1,72 @@
 from . import properties as props
 from .. import data
 
-mountains = data.Thing({
+def Thing(init, *other_inits):
+	init.update(*other_inits)
+	return data.Thing(init)
+
+_creature = {
+	props.action_points: 1,
+	props.population: 1,
+	props.inventory: set()
+}
+
+_weapon = {
+	props.graphics: props.Graphics(char=')', colour=0xffffff)
+}
+
+_missile = {
+	props.graphics: props.Graphics(char='/', colour=0xffffff)
+}
+
+_device = {
+	props.graphics: props.Graphics(char='?', colour=0xffffff)
+}
+
+mountains = Thing({
 	props.name: "mountains",
 	props.graphics: props.Graphics(char='^', colour=0xcfc19a)
 })
-grassland = data.Thing({
+grassland = Thing({
 	props.name: "grassland",
 	props.graphics: props.Graphics(char='.', colour=0x10ad80),
 	props.walk_over_ap: 1
 })
-desert = data.Thing({
+desert = Thing({
 	props.name: "desert",
 	props.graphics: props.Graphics(char='.', colour=0xffcf6d),
 	props.walk_over_ap: 1
 })
-forest = data.Thing({
+forest = Thing({
 	props.name: "forest",
 	props.graphics: props.Graphics(char='&', colour=0x345132),
 	props.walk_over_ap: 2
 })
-water = data.Thing({
+water = Thing({
 	props.name: "water",
 	props.graphics: props.Graphics(char='~', colour=0x22b6f2)
 })
-road = data.Thing({
+road = Thing({
 	props.name: "road",
 	props.graphics: props.Graphics(char='.', colour=0x222222),
 	props.walk_over_ap: 0.5
 })
 
 def city():
-	return data.Thing({
+	return Thing({
 		props.name: "city",
 		props.graphics: props.Graphics(char='#', colour=0xaaaaaa)
 	})
 
 def goal():
-	return data.Thing({
+	return Thing({
 		props.name: "Wastrl",
 		props.graphics: props.Graphics(char='#', colour=0xffffff),
 		props.is_goal: True
 	})
 
 def player():
-	return data.Thing({
+	return Thing(_creature, {
 		props.name: "player",
 		props.graphics: props.Graphics(char='@', colour=0xffffff),
 		props.is_player: True,
@@ -54,7 +76,7 @@ def player():
 	})
 
 def ratling():
-	return data.Thing({
+	return Thing(_creature, {
 		props.name: "ratling",
 		props.graphics: props.Graphics(char='r', colour=0xffffff),
 		props.action_points: 3,
@@ -63,31 +85,26 @@ def ratling():
 	})
 
 def missile_of_kaboom():
-	return data.Thing({
-		props.name: "missile of kaboom",
-		props.graphics: props.Graphics(char=')', colour=0xffffff)
+	return Thing(_missile, {
+		props.name: "missile of kaboom"
 	})
 
 def missile_of_fire_ball():
-	return data.Thing({
-		props.name: "missile of fire ball",
-		props.graphics: props.Graphics(char=')', colour=0xffffff)
+	return Thing(_missile, {
+		props.name: "missile of fire ball"
 	})
 
 def missile_of_nuclear_warhead():
-	return data.Thing({
-		props.name: "missile of nuclear warhead",
-		props.graphics: props.Graphics(char=')', colour=0xffffff)
+	return Thing(_missile, {
+		props.name: "missile of nuclear warhead"
 	})
 
 def device_of_mapping():
-	return data.Thing({
-		props.name: "device of mapping",
-		props.graphics: props.Graphics(char='/', colour=0xffffff)
+	return Thing(_device, {
+		props.name: "device of mapping"
 	})
 
 def device_of_restoration():
-	return data.Thing({
-		props.name: "device of restoration",
-		props.graphics: props.Graphics(char='/', colour=0xffffff)
+	return Thing(_device, {
+		props.name: "device of restoration"
 	})
