88dedadd7851a63eca2318e7761e4ebbae2f40f1
Max Whitney <mwhitney@alumni.sfu.ca>
2018-03-09 12:46:52 +0800

    Some performance improvements for AI pathfinding.

--- wastrl/game/ai.py
+++ wastrl/game/ai.py
@@ -11,30 +11,45 @@ class DijkstraMap:
 	__slots__ = (
 		'_terrain',
 		'_max_dist_to_player',
+		'_walk_map',
 		'_map'
 	)
 
-	def __init__(self, terrain, max_dist_to_player=50):
+	def __init__(self, terrain, max_dist_to_player=40):
 		self._terrain = terrain
 		self._max_dist_to_player = max_dist_to_player
+		self._walk_map = self.make_walk_map(terrain)
 		self._map = numpy.zeros(shape=tuple(reversed(self._terrain.dim)))
+
+	def make_walk_map(self, terrain, block_cost=float('inf')):
+		walk = numpy.zeros(shape=tuple(reversed(terrain.dim)))
+		for x in range(terrain.dim[0]):
+			for y in range(terrain.dim[1]):
+				t = terrain[x, y]
+				if t in props.walk_over_ap:
+					c = props.walk_over_ap[t]
+				else:
+					c = block_cost
+				walk[y, x] = c
+		return walk
 
 	def update(self, player_pos):
 		self._map[:] = out_of_range_value
-		def touch(pos, dist):
-			if dist > self._max_dist_to_player:
-				return False
-			else:
-				x, y = pos
-				self._map[y, x] = dist
-				return True
-		start_time = time.time()
-		tilemap.dijkstra(
-			graph = self._terrain,
-			starts = (player_pos,),
-			touch = touch,
-			cost = utils.walk_cost(self._terrain)
-		)
+		fringe = tilemap.SearchFringe()
+		fringe.put(player_pos, 0)
+		while not fringe.is_empty():
+			node = fringe.pop()
+			node_dist = fringe.get(node)
+			x, y = node
+			self._map[y, x] = node_dist
+			for neighbour in tilemap.neighbours(node):
+				x, y = neighbour
+				if x >= 0 and y >= 0 and x < self._walk_map.shape[1] and y < self._walk_map.shape[0]:
+					new_dist = node_dist + self._walk_map[y, x]
+					if new_dist <= self._max_dist_to_player:
+						old_dist = fringe.get(neighbour)
+						if old_dist is None or new_dist < old_dist:
+							fringe.put(neighbour, new_dist)
 
 	def move_from(self, pos):
 		x, y = pos
